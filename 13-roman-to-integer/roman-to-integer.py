39dca338e5f2506139cf7a4c74ef60be4fd6a441
tsenthil5 <48156963+tsenthil5@users.noreply.github.com>
2024-06-09 18:59:42 -0500

    Time: 58 ms (7.33%) | Memory: 16.6 MB (57.58%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -15,14 +15,18 @@ class Solution:
                 return 0
             if ptr == len(s)-1:
                 return hashmap[s[ptr]]
-
+            if s[ptr] in cache:
+                return cache[s[ptr]]
             if hashmap[s[ptr]] < hashmap[s[ptr+1]]:
-                return hashmap[s[ptr+1]] - hashmap[s[ptr]] + backtracking(ptr+2)
+                cache[s[ptr]] = hashmap[s[ptr+1]] - hashmap[s[ptr]] + backtracking(ptr+2)
+                return cache[s[ptr]]
 
 
-            return hashmap[s[ptr]] + backtracking(ptr+1)
+            cache[s[ptr]] = hashmap[s[ptr]] + backtracking(ptr+1)
+            return cache[s[ptr]]
 
 
+        cache = {}
         return backtracking(0)
 
             
