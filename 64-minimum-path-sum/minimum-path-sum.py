79a050211cdf95c20b571d51bca8893b457d0eb7
tsenthil5 <48156963+tsenthil5@users.noreply.github.com>
2024-08-08 08:37:15 -0500

    Time: 111 ms (6.24%) | Memory: 22.2 MB (6.01%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,23 +1,22 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        def dfs(row, col):
-            if (row, col) in cache:
-                return cache[(row, col)]
-            if row >= len(grid) or col >= len(grid[0]):
+        '''
+        123
+        456
+        '''
+        def dp(startIdx, endIdx):
+            if startIdx < 0 or endIdx < 0 or startIdx >= len(grid) or endIdx >= len(grid[0]):
                 return float('inf')
-            
-            if row == len(grid)-1 and col == len(grid[0])-1:
-                return grid[row][col]
+            if (startIdx, endIdx) in cache:
+                return cache[(startIdx, endIdx)]
 
-            
-            minVal = float('inf')
-            minVal = min(minVal, dfs(row+1,col),
-            dfs(row, col+1))
-            cache[(row, col)] = minVal + grid[row][col]
-            
-            return minVal + grid[row][col]
+            if startIdx == len(grid)-1 and endIdx == len(grid[0])-1:
+                return grid[startIdx][endIdx]
 
+            cache[(startIdx, endIdx)] = min(dp(startIdx+1, endIdx), dp(startIdx, endIdx+1)) + grid[startIdx][endIdx]
+            return cache[(startIdx, endIdx)]
+            
 
         cache = {}
-        return dfs(0,0)
+        return dp(0, 0)
         
