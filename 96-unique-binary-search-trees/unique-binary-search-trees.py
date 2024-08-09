6a7fce465bf20622f525298056003cc208207ae7
tsenthil5 <48156963+tsenthil5@users.noreply.github.com>
2024-08-09 04:11:53 -0500

    Time: 44 ms (6.84%) | Memory: 16.5 MB (18.12%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -1,5 +1,15 @@
 class Solution:
     @cache
     def numTrees(self, n: int) -> int:
-        if n <= 1: return 1
-        return sum(self.numTrees(i-1) * self.numTrees(n-i) for i in range(1, n+1))
+        if n<=1:
+            return 1
+
+
+        total = 0
+        for i in range(1,n+1):
+            leftChild = self.numTrees(i-1)
+            rightChild = self.numTrees(n-i)
+            total+=leftChild*rightChild
+
+        return total
+        
