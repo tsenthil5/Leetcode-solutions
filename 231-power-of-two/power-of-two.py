6338a290a84cdd9e65015e7ac7e8e0793b8fbd87
tsenthil5 <48156963+tsenthil5@users.noreply.github.com>
2024-09-11 17:50:46 -0500

    Time: 27 ms (95.73%) | Memory: 16.6 MB (26.08%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while(n!=1):
+            n = n/2
+            if n % 1 != 0 or n == 0:
+                return False
+
+        return True
+        
