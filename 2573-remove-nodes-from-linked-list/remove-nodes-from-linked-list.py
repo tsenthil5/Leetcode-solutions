ef68b0af6d76339910b75d4ca22edac037be9380
tsenthil5 <48156963+tsenthil5@users.noreply.github.com>
2024-09-20 22:01:57 -0500

    Time: 414 ms (69.23%) | Memory: 51.1 MB (69.51%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def linkedListRev(head):
+            pre, cur = head, head.next
+            
+            while cur:
+                
+                temp = cur.next
+                cur.next = pre
+                if pre == head:
+                    pre.next = None
+                pre = cur
+                cur = temp
+            return pre
+
+
+        currMax = -1
+        newHead = linkedListRev(head)
+        startPtr = newHead
+        prevHead = newHead
+        
+        while newHead:
+            if newHead.val >= currMax:
+                currMax = newHead.val
+                prevHead = newHead
+
+            else:
+                prevHead.next = newHead.next
+
+            
+            newHead = newHead.next
+
+        return (linkedListRev(startPtr))
+
+
+        
